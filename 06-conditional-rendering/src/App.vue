c7f91b89114689afcf43f5e691d1bd544b2629b0
hoaithu <hoaithu.hb2002@gmail.com>
2026-04-26 10:26:33 +0700

    [FE]: practice binding class + style; conditional rendering

---
+++ 06-conditional-rendering/src/App.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+const isLogin = ref(true)
+const userStatus = ref('online')
+
+const toggleLogin = () => {
+  isLogin.value = !isLogin.value
+}
+const handleChangeStatus = () => {
+  userStatus.value =
+    userStatus.value === 'online' ? 'away' : userStatus.value === 'away' ? 'busy' : 'online'
+}
+</script>
+
+<template>
+  <div>
+    <!-- v-if / v-else / v-else-if -->
+    <div>
+      <p v-if="isLogin">Chào mừng người dùng đã đăng nhập</p>
+      <p v-else>Chào mừng khách</p>
+      <button @click="toggleLogin">
+        {{ isLogin ? 'Logout' : 'Login' }}
+      </button>
+      <br />
+      <p v-if="userStatus === 'online'">Người dùng đang online</p>
+      <p v-else-if="userStatus === 'away'">Người dùng đang vắng mặt</p>
+      <p v-else-if="userStatus === 'busy'">Người dùng đang bận</p>
+      <button @click="handleChangeStatus">
+        {{
+          userStatus === 'online' ? 'Set Away' : userStatus === 'away' ? 'Set Busy' : 'Set Online'
+        }}
+      </button>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
